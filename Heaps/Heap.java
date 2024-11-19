04cce1838ac9245134b8c25a2cb9fb270800b6ed
Reallyishan009 <ishantripathidoc@gmail.com>
2024-11-19 20:06:54 +0530

    heap

---
+++ Heaps/Heap.java
@@ -0,0 +1,39 @@
+package Heaps;
+import java.util.ArrayList;
+
+public class Heap {
+    public static void insert(ArrayList<Integer> list, int val) {
+        list.add(val);
+        upheapify(list, list.size() - 1);
+    }
+
+    
+    public static void upheapify(ArrayList<Integer> list, int c) {
+        int p = (c - 1) / 2;
+
+        while (p >= 0 && list.get(p) > list.get(c)) {
+            swap(list, p, c);
+            c = p;
+            p = (c - 1) / 2;
+        }
+    }
+
+    
+    public static void swap(ArrayList<Integer> list, int p, int c) {
+        int temp = list.get(p);
+        list.set(p, list.get(c));
+        list.set(c, temp);
+    }
+
+    
+    public static void main(String[] args) {
+        ArrayList<Integer> heap = new ArrayList<>();
+        int[] values = {5, 3, 8, 2, 1, 7};
+        
+        for (int val : values) {
+            insert(heap, val);
+        }
+        
+        System.out.println(heap);
+    }
+}
